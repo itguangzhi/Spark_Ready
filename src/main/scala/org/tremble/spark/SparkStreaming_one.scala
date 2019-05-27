46ff4500a79d27a99ec18b014a8be1a5bf98f29e
guangzhidaxiongdi <itguangzhi@163.com>
2019-05-27 17:35:14 +0800

    增加了点东西，也没看懂啥，又是荒芜的一天

--- src/main/scala/org/tremble/spark/SparkStreaming_one.scala
+++ src/main/scala/org/tremble/spark/SparkStreaming_one.scala
@@ -1,9 +1,6 @@
 package org.tremble.spark
 
 
-
-import java.util.logging.Logger
-
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -11,7 +8,7 @@ import org.apache.spark.{SparkConf, SparkContext}
   * Created by UCS-TREMBLE on 2019-04-21.
   */
 object SparkStreaming_one {
-  val log4j = Logger.
+
   def main(args: Array[String]): Unit = {
     // StreamingContext
     val conf = new SparkConf().setAppName("spm").setMaster("local")
